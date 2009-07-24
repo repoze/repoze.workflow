638654e7fb4a24fce98c06a5f34f893c06908436
Chris McDonough <chrism@agendaless.com>
2009-07-24 15:05:35 +0000

    - Allow ``add`` method to accept arbitrary keyword arguments.  These   keyword arguments are now passed as ``**kw`` to transition functions   as well as ``before_transition`` and ``after_transition``.  This   allows arbitrary metadata (such as security and UI information) to   be associated with a transition.
    - The ``transitions`` method now returns all transitions, including
      "catch-all" transitions (transitions with a transition_id of None).

--- repoze/workflow/statemachine.py
+++ repoze/workflow/statemachine.py
@@ -21,10 +21,10 @@ class StateMachine(object):
     * (state, None) is checked next.
 
     The transition function must be of the following form:
-    * function(current_state, new_state, transition_id, context)
+    * function(current_state, new_state, transition_id, context, **kw)
 
-    It is highly recommended that all transition functions be module
-    level callables to facilitate issues related to StateMachine
+    It is recommended that all transition functions be module level
+    callables to facilitate issues related to StateMachine
     persistence.
     """
     implements(IStateMachine)
@@ -46,8 +46,8 @@ class StateMachine(object):
         self.state_attr = state_attr
         self.initial_state = initial_state
 
-    def add(self, state, transition_id, newstate, transition_fn):
-        self.states[(state, transition_id)] = (newstate, transition_fn)
+    def add(self, state, transition_id, newstate, transition_fn, **kw):
+        self.states[(state, transition_id)] = (newstate, transition_fn, kw)
 
     def execute(self, context, transition_id):
         state = getattr(context, self.state_attr, _marker) 
@@ -58,17 +58,17 @@ class StateMachine(object):
         newstate = None
         # exact state match?
         if si in self.states:
-            newstate, transition_fn = self.states[si]
+            newstate, transition_fn, kw = self.states[si]
         # no exact match, how about a None (catch-all) match?
         elif sn in self.states:
-            newstate, transition_fn = self.states[sn]
+            newstate, transition_fn, kw = self.states[sn]
         if newstate is None:
             raise StateMachineError(
                 'No transition from %r using transition %r'
                     % (state, transition_id))
-        self.before_transition(state, newstate, transition_id, context)
-        transition_fn(state, newstate, transition_id, context)
-        self.after_transition(state, newstate, transition_id, context)
+        self.before_transition(state, newstate, transition_id, context, **kw)
+        transition_fn(state, newstate, transition_id, context, **kw)
+        self.after_transition(state, newstate, transition_id, context, **kw)
         setattr(context, self.state_attr, newstate)
 
     def state_of(self, context):
@@ -79,11 +79,25 @@ class StateMachine(object):
         if from_state is None:
             from_state = self.state_of(context)
         transitions = [t_id for (state, t_id) in self.states.keys()
-                       if state == from_state and t_id is not None]
+                       if state == from_state]
         return transitions
 
-    def before_transition(self, state, newstate, transition_id, context):
+    def transition_info(self, context, from_state=None):
+        if from_state is None:
+            from_state = self.state_of(context)
+        L = []
+        for (state,t_id), (newstate,transition_fn,kw) in self.states.items():
+            if state == from_state:
+                newkw = {}
+                newkw.update(kw)
+                newkw['transition_id'] = t_id
+                newkw['from_state'] = state
+                newkw['to_state'] = newstate
+                L.append(newkw)
+        return L
+
+    def before_transition(self, state, newstate, transition_id, context, **kw):
         pass
 
-    def after_transition(self, state, newstate, transition_id, context):
+    def after_transition(self, state, newstate, transition_id, context, **kw):
         pass
