2523dd5fdce99a0d5b0db718a6ddb6396a68d98a
Chris McDonough <chrism@agendaless.com>
2009-07-27 22:56:33 +0000

    Structure for doing KARL-like workflow as a separate package.

---
+++ setup.py
@@ -0,0 +1,56 @@
+##############################################################################
+#
+# Copyright (c) 2008 Agendaless Consulting and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the BSD-like license at
+# http://www.repoze.org/LICENSE.txt.  A copy of the license should accompany
+# this distribution.  THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL
+# EXPRESS OR IMPLIED WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND
+# FITNESS FOR A PARTICULAR PURPOSE
+#
+##############################################################################
+
+__version__ = '0.0'
+
+import os
+
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'repoze.workflow',
+    'repoze.bfg'
+    ]
+
+setup(name='repoze.bfg.workflow',
+      version=__version__,
+      description='Content workflow for repoze.bfg',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        ],
+      keywords='web repoze bfg workflow',
+      author="Agendaless Consulting",
+      author_email="repoze-dev@lists.repoze.org",
+      url="http://www.repoze.org",
+      license="BSD-derived (http://www.repoze.org/LICENSE.txt)",
+      packages=find_packages(),
+      include_package_data=True,
+      namespace_packages=['repoze', 'repoze.bfg'],
+      zip_safe=False,
+      tests_require = requires,
+      install_requires= requires,
+      test_suite="repoze.bfg.workflow",
+      entry_points = """
+      """
+      )
+
