45cec8028b4527293cb033c32f27daffdfbde523
Chris McDonough <chrism@agendaless.com>
2009-07-28 17:01:11 +0000

    Add IWorkflowFactory to the API.

--- repoze/bfg/workflow/__init__.py
+++ repoze/bfg/workflow/__init__.py
@@ -1,6 +1,7 @@
 from repoze.bfg.workflow.workflow import Workflow # API
 from repoze.bfg.workflow.statemachine import StateMachine #API
 from repoze.bfg.workflow.interfaces import IWorkflow # API
+from repoze.bfg.workflow.interfaces import IWorkflowFactory # API
 from repoze.bfg.workflow.interfaces import IStateMachine #API
 
 
