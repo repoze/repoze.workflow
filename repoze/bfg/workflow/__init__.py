fe9976743f4939a31dc1070b3bde25f501b433db
Chris McDonough <chrism@agendaless.com>
2009-07-29 01:14:19 +0000

    Make StateMachineError importable.

--- repoze/bfg/workflow/__init__.py
+++ repoze/bfg/workflow/__init__.py
@@ -1,5 +1,6 @@
 from repoze.bfg.workflow.workflow import Workflow # API
 from repoze.bfg.workflow.statemachine import StateMachine #API
+from repoze.bfg.workflow.statemachine import StateMachineError #API
 from repoze.bfg.workflow.interfaces import IWorkflow # API
 from repoze.bfg.workflow.interfaces import IWorkflowFactory # API
 from repoze.bfg.workflow.interfaces import IStateMachine #API
