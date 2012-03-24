d60e0f58c42dc449bc5609d4188cea6495acc4ff
Tres Seaver <tseaver@palladion.com>
2012-03-24 13:08:06 -0400

    Add 'dev' alias for setup.py.

--- setup.py
+++ setup.py
@@ -38,27 +38,32 @@ tests_requires = requires + [
     'repoze.sphinx.autointerface',
     ]
 
-setup(name='repoze.workflow',
-      version=__version__,
-      description='Content workflow for repoze.bfg',
-      long_description=README + '\n\n' +  CHANGES,
-      classifiers=[
+testing_extras = tests_requires + ['nose', 'coverage']
+
+setup(
+    name='repoze.workflow',
+    version=__version__,
+    description='Content workflow for repoze.bfg',
+    long_description=README + '\n\n' +  CHANGES,
+    classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
-        ],
-      keywords='web repoze workflow',
-      author="Agendaless Consulting",
-      author_email="repoze-dev@lists.repoze.org",
-      url="http://www.repoze.org",
-      license="BSD-derived (http://www.repoze.org/LICENSE.txt)",
-      packages=find_packages(),
-      include_package_data=True,
-      namespace_packages=['repoze'],
-      zip_safe=False,
-      tests_require = tests_requires,
-      install_requires= requires,
-      test_suite="repoze.workflow",
-      entry_points = """
-      """
-      )
-
+    ],
+    keywords='web repoze workflow',
+    author="Agendaless Consulting",
+    author_email="repoze-dev@lists.repoze.org",
+    url="http://www.repoze.org",
+    license="BSD-derived (http://www.repoze.org/LICENSE.txt)",
+    packages=find_packages(),
+    include_package_data=True,
+    namespace_packages=['repoze'],
+    zip_safe=False,
+    tests_require = tests_requires,
+    install_requires= requires,
+    test_suite="repoze.workflow",
+    entry_points = """
+    """,
+    extras_require={
+        'testing': testing_extras,
+    },
+)
